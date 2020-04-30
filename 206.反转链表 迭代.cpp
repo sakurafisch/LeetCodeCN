2e49632de97db244110d3c3a594c9f23a50c55a7
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-04-30 23:15:18 +0800

    206.反转链表 | 迭代

---
+++ 206.反转链表 迭代.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        ListNode* lat = nullptr;
+        while (cur != nullptr) {
+            lat = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = lat;
+        }
+        return pre;
+    }
+};
+// @lc code=end
+
