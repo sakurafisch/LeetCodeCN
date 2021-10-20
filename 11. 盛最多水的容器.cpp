34c7c4156e4fb44370475ae9ac3df9d55d322509
sakurafisch <sakurafisch@gmail.com>
2021-10-20 16:44:16 +0800

    feat: 11.盛水最多的容器

---
+++ 11. 盛最多水的容器.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+using std::vector;
+using std::min;
+using std::max;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0;
+        int left = 0, right = height.size() - 1;
+        while (left < right) {
+            int area = min(height[left], height[right]) * (right - left);
+            ans = max(ans, area);
+            if (height[left] < height[right]) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        return ans;
+    }
+};
