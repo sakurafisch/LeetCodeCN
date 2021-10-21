69ea1426540d080f0afb3f343af185dee616f4f9
sakurafisch <sakurafisch@gmail.com>
2021-10-21 22:37:18 +0800

    feat: 添加Golang解法 28.实现strStr()

---
+++ 28.实现 strStr().go
@@ -0,0 +1,22 @@
+package leetcodecn
+
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(haystack) < len(needle) {
+		return -1
+	}
+	var i, j int
+	for i = 0; i < len(haystack)-len(needle)+1; i++ {
+		for j = 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+		}
+		if len(needle) == j {
+			return i
+		}
+	}
+	return -1
+}
