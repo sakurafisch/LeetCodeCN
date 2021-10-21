b5f7c50341c1d7e8122d0c09b16db693a7a691ab
sakurafisch <sakurafisch@gmail.com>
2021-10-21 16:10:36 +0800

    refactor: update 14.最大公共前缀

--- 14.最长公共前缀.cpp
+++ 14.最长公共前缀.cpp
@@ -6,14 +6,11 @@ using std::string;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.size() == 0) {
-            return 0;
-        }
         string ans = strs[0];
         for (int i = 0; i < strs.size(); ++i) {
             for (int j = 0; j < ans.length(); ++j) {
                 if (strs[i][j] != ans[j]) {
-                    ans = ans.substr(0, j);
+                    ans = strs[0].substr(0, j);
                     break;
                 }
             }
