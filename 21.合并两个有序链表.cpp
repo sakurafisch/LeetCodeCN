be0856eb618fdc3fb739c5a4fd3e5fa70907fd76
sakurafisch <sakurafisch@gmail.com>
2021-10-21 21:31:10 +0800

    refactor: 更新解法 21.合并两个有序链表

--- 21.合并两个有序链表.cpp
+++ 21.合并两个有序链表.cpp
@@ -1,41 +1,37 @@
-#include <iostream>
-using namespace std;
+// Definition for singly-linked list.
 struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x) : val(x), next(NULL) {}
+   int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
-
+ 
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (l1 == NULL) return l2;
-        if (l2 == NULL) return l1;
-        ListNode* l3 = new ListNode(min(l1->val, l2->val));
-        ListNode *ptr3 = l3;
-        ListNode *ptr1 = l1->val < l2->val ? l1->next : l1;
-        ListNode* ptr2 = l1->val < l2->val ? l2 : l2->next;
-        while (ptr1 != NULL && ptr2 != NULL) {
-                if (ptr1->val < ptr2->val) {
-                    ptr3->next = new ListNode(ptr1->val);
-                    ptr1 = ptr1->next;
-                    ptr3 = ptr3->next;
-                } else {
-                    ptr3->next = new ListNode(ptr2->val);
-                    ptr2 = ptr2->next;
-                    ptr3 = ptr3->next;
-                }
+        ListNode* h = new ListNode(0);
+        ListNode* p = h;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
         }
-        while (ptr1 != NULL) {
-            ptr3->next = new ListNode(ptr1->val);
-            ptr1 = ptr1->next;
-            ptr3 = ptr3->next;
+        while (l1) {
+            p->next = l1;
+            l1 = l1->next;
+            p = p->next;
         }
-        while (ptr2 != NULL) {
-            ptr3->next = new ListNode(ptr2->val);
-            ptr2 = ptr2->next;
-            ptr3 = ptr3->next;
+        while (l2) {
+            p->next = l2;
+            l2 = l2->next;
+            p = p->next;
         }
-        return l3;
+        return h->next;
     }
 };
