31f6dbaba6ed5563b6a4dbb39ee188a569c3339a
sakurafisch <sakurafisch@gmail.com>
2022-01-14 00:38:16 +0800

    feat: 215.数组中的第K个最大元素.cc

---
+++ 215.数组中的第K个最大元素.cc
@@ -0,0 +1,17 @@
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        multiset<int> s;
+        for (int i = 0; i < nums.size(); ++i) {
+            s.insert(nums[i]);
+            if (s.size() > k)
+                s.erase(s.begin());
+        }
+        return *s.begin();
+    }
+};
