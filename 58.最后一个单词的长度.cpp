607dee942b4cff4a90ecb0d30740f8804d0039fc
sakurafisch <sakurafisch@gmail.com>
2022-01-20 23:05:51 +0800

    refactor: 58 题逆序找第一个单词就行

--- 58.最后一个单词的长度.cpp
+++ 58.最后一个单词的长度.cpp
@@ -1,33 +1,22 @@
-/*
- * @lc app=leetcode.cn id=58 lang=cpp
- *
- * [58] 最后一个单词的长度
- */
-
-// @lc code=start
-
-#include <iostream>
 #include <string>
 using namespace std;
+
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        string pre = "";
-        string ans = "";
-        for(int i = 0; i < s.length(); ++i) {
-            if(s.at(i) != ' ') {
-                ans += s.at(i);
-            } else {
-                if (ans != "") {
-                    pre = ans;
-                    ans = "";
-                } else {
-                    continue;
-                }
-            }
+        int ans = 0;
+        int i = s.size() - 1;
+        while (i >= 0) {
+            if (s[i] != ' ')
+                break;
+            --i;
         }
-        return ans == "" ? pre.length() : ans.length();
+        while (i >= 0) {
+            if (s[i] == ' ')
+                break;
+            ++ans;
+            --i;
+        }
+        return ans;
     }
 };
-// @lc code=end
-
