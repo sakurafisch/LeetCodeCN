fd5092308b9cb33d5c8ce01de740a5869ee834df
sakurafisch <sakurafisch@gmail.com>
2022-01-20 22:46:02 +0800

    feat: 57.插入区间.cc

---
+++ 57.插入区间.cc
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        intervals.push_back(newInterval);
+        if (intervals.empty())
+            return ans;
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); ++i)
+            if (ans.back()[1] < intervals[i][0])
+                ans.push_back(intervals[i]);
+            else
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        return ans;
+    }
+};
