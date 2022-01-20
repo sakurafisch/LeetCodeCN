cb3d390310d3427aca89e15951ca74decaeb16d7
sakurafisch <sakurafisch@gmail.com>
2022-01-20 22:36:18 +0800

    feat: 56.合并区间.cc

---
+++ 56.合并区间.cc
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        if (intervals.empty())
+            return ans;
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); ++i)
+            if (ans.back()[1] < intervals[i][0])
+                ans.push_back(intervals[i]);
+            else
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        return ans;
+    }
+};
