19df96e4f104707c3f29d3b4052f7a0327b57822
sakurafisch <sakurafisch@gmail.com>
2023-01-23 22:21:23 +0800

    feat: 23.合并K个升序链表.cc

---
+++ 23.合并K个升序链表.cc
@@ -0,0 +1,32 @@
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        auto cmp = [](const ListNode* x, const ListNode* y) -> bool { return x->val > y->val; };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
+        for (int i = 0; i < lists.size(); ++i) {
+            if (lists[i] != nullptr) {
+                pq.push(lists[i]);
+            }
+        }
+        ListNode* dummy = new ListNode(0);
+        ListNode* pre = dummy;
+        while (!pq.empty()) {
+            ListNode* node = pq.top(); pq.pop();
+            pre->next = node;
+            pre = pre->next;
+            if (node->next != nullptr) {
+                pq.push(node->next);
+            }
+        }
+        return dummy->next;
+    }
+};
